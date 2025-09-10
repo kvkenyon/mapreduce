2d25373c90b0e018ba7a06e3c976d8a2c8aad879
Kevin <kvkenyon@gmail.com>
2025-09-10 13:39:46 -0500

    Implement and test file splitter ensuring not word truncation

--- src/file_splitter.rs
+++ src/file_splitter.rs
@@ -1,8 +1,8 @@
 //! src/file_splitter.rs
-use std::fs::{self, File};
-use std::{os::unix::fs::MetadataExt, path::PathBuf};
-
 use anyhow::Context;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::path::PathBuf;
 
 #[allow(unused)]
 pub struct FileSplitter {
@@ -35,29 +35,43 @@ impl FileSplitter {
             )));
         }
 
-        let metadata = self.path.metadata().context("Failed to extract metadata")?;
-        let size_in_bytes = metadata.size();
+        let in_file = File::open(&self.path).context("Failed to open input file")?;
 
-        let mut out_file_count = size_in_bytes / self.split_size_in_bytes;
-        if size_in_bytes % self.split_size_in_bytes != 0 {
-            out_file_count += 1;
-        }
+        let buf_reader = BufReader::new(&in_file);
+        let mut results: Vec<PathBuf> = Vec::new();
+        let (mut curr_out_file, mut path) = self.create_new_out_file("0")?;
+        let mut curr_out_file_size = 0;
+        let mut curr_suffix = 0;
+        for line in buf_reader.lines() {
+            let line = line.context("Failed to read line")?;
+            let line_size = line.len();
 
-        let _in_file = fs::read_to_string(&self.path).context("Failed to read input file")?;
-        let mut results: Vec<PathBuf> = Vec::with_capacity(out_file_count as usize);
-        for i in 0..out_file_count {
-            let mut result = PathBuf::from(&self.out_path);
-            result.push(format!("{}_{i}", self.out_file_name));
+            if line_size + curr_out_file_size > self.split_size_in_bytes as usize {
+                results.push(path);
+                // Create next file and reset state
+                curr_suffix += 1;
+                (curr_out_file, path) = self.create_new_out_file(&curr_suffix.to_string())?;
+                curr_out_file_size = 0;
+            }
 
-            let _file = File::create(&result).context(format!(
-                "Failed to create file at: {}",
-                result.to_str().context("Failed to parse file path")?
-            ))?;
-
-            results.push(result);
+            curr_out_file
+                .write(line.as_bytes())
+                .context("Failed to write line to out file")?;
+            curr_out_file_size += line_size;
         }
 
         Ok(results)
+    }
+
+    fn create_new_out_file(&self, suffix: &str) -> Result<(File, PathBuf), anyhow::Error> {
+        let name = format!("{}_{suffix}", self.out_file_name);
+        let mut path = PathBuf::from(&self.out_path);
+        path.push(name);
+        let file = File::create(&path).context(format!(
+            "Failed to create file at: {}",
+            path.to_str().context("Failed to parse file path")?
+        ))?;
+        Ok((file, path))
     }
 }
 
@@ -67,7 +81,7 @@ mod tests {
 
     use super::*;
     use crate::test_utils::test_data_dir;
-    use claims::assert_err;
+    use claims::{assert_err, assert_le};
 
     #[test]
     fn should_fail_if_in_file_doesnt_exist() {
@@ -93,23 +107,51 @@ mod tests {
     #[test]
     fn should_store_the_files_at_out_path_with_out_file_name_with_index() {
         let mut filename = test_data_dir();
+
         filename.push("small_test.txt");
 
-        let file_size = filename.metadata().unwrap().size();
+        let file_size = fs::metadata(&filename)
+            .expect("Failed to read metadata")
+            .len();
 
         assert_eq!(file_size, 2633);
 
-        let out_path = PathBuf::from("/tmp/mapreduce");
+        let out_path = PathBuf::from(format!("/tmp/mapreduce/{}", uuid::Uuid::new_v4()));
+        fs::create_dir_all(&out_path).expect("Failed to create test directory");
 
-        let splitter = FileSplitter::new(filename, 100, "test_out".to_string(), out_path);
+        let splitter = FileSplitter::new(filename, 100, "test_out".to_string(), out_path.clone());
 
         let out_files = splitter.split().expect("Failed to split");
 
-        assert_eq!(out_files.len(), 27);
+        for (i, path) in out_files.iter().enumerate() {
+            let name = format!("test_out_{i}");
+            assert_eq!(path.file_name().unwrap().to_str().unwrap(), name);
+        }
 
-        for file in out_files {
-            assert!(file.exists());
-            fs::remove_file(file).expect("Failed to remove file");
+        fs::remove_dir_all(out_path).expect("Failed to delete dirs");
+    }
+
+    #[test]
+    fn should_write_the_correct_amount_of_data_to_outfiles() {
+        let mut filename = test_data_dir();
+        filename.push("small_test.txt");
+
+        let file_size = fs::metadata(&filename)
+            .expect("Failed to read metadata")
+            .len();
+
+        assert_eq!(file_size, 2633);
+
+        let out_path = PathBuf::from(format!("/tmp/mapreduce/{}", uuid::Uuid::new_v4()));
+
+        fs::create_dir_all(&out_path).expect("Failed to create test directory");
+
+        let splitter = FileSplitter::new(filename, 100, "test_out".to_string(), out_path);
+        let out_files = splitter.split().expect("Failed to split");
+
+        for path in out_files {
+            let size = fs::metadata(path).unwrap().len();
+            assert_le!(size, 100);
         }
     }
 }
